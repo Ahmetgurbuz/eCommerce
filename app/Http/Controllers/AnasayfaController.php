27b9dcf000a7f64245556b94bd209c1fbeae550a
ahmet gurbuz <ahmetgurbuz141@outlook.com>
2021-02-23 19:46:32 +0300

    Laravel mvc eticaret

---
+++ app/Http/Controllers/AnasayfaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Resources\Views;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AnasayfaController extends Controller
+{
+    public function Index()
+    {
+        return View('ansayfa');
+
+    }
+
+
+}
